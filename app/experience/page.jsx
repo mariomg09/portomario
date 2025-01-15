0b162213b7ddcf4c4a120c0f8d1803bb42bb836b
Mario Matthews Gunawan <mario.matthews@ui.ac.id>
2025-01-15 12:15:57 +0700

    Manual data (not using prisma)

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -1,16 +1,108 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import Card from "./card";
 import Tag from "./tag";
 import { motion, useInView } from "framer-motion";
 
+const experiencesData = [
+  {
+    id: 1,
+    title: "Tata Sarana Mandiri Technology (TSM Technology)",
+    positions: [
+      {
+        description: "Software Engineer Internship",
+        time: "Jan 2025 - Feb 2025",
+      },
+    ],
+    image: "/Experience/tsm.jpg",
+    tag: ["All", "Professional"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Ikatan Mahasiswa Elektro FTUI 2024",
+    positions: [
+      {
+        description: "President",
+        time: "Jan 2024 - Dec 2024",
+      },
+      {
+        description: "Staff of Communication and Information",
+        time: "Jan 2023 - Dec 2023",
+      },
+    ],
+    image: "/Experience/ime.jpg",
+    tag: ["All", "Organizational"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "MADK DTE FTUI 2024",
+    positions: [
+      {
+        description: "Steering Committee",
+        time: "Feb 2024 - Aug 2024",
+      },
+    ],
+    image: "/Experience/MADK.JPG",
+    tag: ["All", "Organizational"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Badan Eksekutif Mahasiswa FTUI 2023",
+    positions: [
+      {
+        description: "Staff of Science and Technology",
+        time: "Feb 2023 - Dec 2025",
+      },
+    ],
+    image: "/Experience/bem.jpg",
+    tag: ["All", "Organizational"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Arjuna IPTEK 2023",
+    positions: [
+      {
+        description: "Project Officer",
+        time: "Apr 2023 - Dec 2023",
+      },
+    ],
+    image: "/Experience/arjuna.jpg",
+    tag: ["All", "Organizational"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Class Representative Assembly SMAN 2 Jakarta",
+    positions: [
+      {
+        description: "President",
+        time: "Aug 2020 - Aug 2021",
+      },
+    ],
+    image: "/Experience/mpk.jpg",
+    tag: ["All", "Organizational"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
 const Experience = () => {
-  const [experiences, setExperiences] = useState([]);
   const [tag, setTag] = useState("All");
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
-  useEffect(() => {
+  {
+    /*
+    useEffect(() => {
     const fetchExperiences = async () => {
       const res = await fetch("/api/Experience");
       const data = await res.json();
@@ -18,12 +110,13 @@ const Experience = () => {
     };
     fetchExperiences();
   }, []);
-
+  */
+  }
   const handleTagChange = (newTag) => {
     setTag(newTag);
   };
 
-  const filteredExperiences = experiences.filter((experience) =>
+  const filteredExperiences = experiencesData.filter((experience) =>
     experience.tag.includes(tag)
   );
 
@@ -66,7 +159,14 @@ const Experience = () => {
           >
             <Card
               title={experience.title}
-              positions={experience.positions}
+              positions={
+                experience.positions || [
+                  {
+                    description: experience.description,
+                    time: experience.time,
+                  },
+                ]
+              }
               imgUrl={experience.image}
               gitUrl={experience.gitUrl}
               previewUrl={experience.previewUrl}
